ef1bcc163dc00d443a7c626e73cdb0ecd80bcfd6
Vishwajith <vish.drck@gmail.com>
2021-09-22 05:07:46 +0530

    Initialized the initial lib

---
+++ projects/svg-icon/src/lib/svg-icon.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vs-svg-icon',
+  template: `
+    <p>
+      svg-icon works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SvgIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/svg-icon/src/lib/svg-icon.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { SvgIconComponent } from './svg-icon.component';
+
+
+
+@NgModule({
+  declarations: [
+    SvgIconComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    SvgIconComponent
+  ]
+})
+export class SvgIconModule { }

52a84ba4ab4a505182d769a29b6227ca9f4de275
Vishwajith <vish.drck@gmail.com>
2021-09-22 06:35:29 +0530

    Added UI Component

--- projects/svg-icon/src/lib/svg-icon.component.ts
+++ projects/svg-icon/src/lib/svg-icon.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'vs-svg-icon',
-  template: `
-    <p>
-      svg-icon works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './svg-icon.component.html',
+  styleUrls: ['./svg-icon.component.css']
 })
 export class SvgIconComponent implements OnInit {
+  @Input() icon='';
+  @Input() parentInlineCss: string = '';
+  @Input() svgInlineCss: string ='';
+  @Input() enableDBlock = false;
+  @Input() height = '24px';
+  @Input() width = '24px';
+  svgPath = '/assets/vs-svg-icons';
+  svgContent: string='';
 
-  constructor() { }
+  constructor() {
+    this.svgContent = '';
+    this.parentInlineCss = '';
+    this.svgInlineCss = '';
+  }
 
   ngOnInit(): void {
+    this.svgInlineCss += `height:${this.height};width:${this.width}`;
+    if(this.enableDBlock) {
+      this.svgInlineCss += 'display:block;';
+    }
   }
 
 }
